a4fd15949e0336dd4fd8d6f924f4481f954db7a0
hspatino <hspatino2@misena.edu.co>
2022-02-15 17:02:20 -0500

    Creamos el location

---
+++ location.py
@@ -0,0 +1,16 @@
+class Location:
+    def __init__(self):
+        self.wandering_location =  {}
+
+    def add_wandering(self, wardering, location):
+        self.wandering_location[wardering] = location
+
+    def move_wandering(self, wandering):
+        delta_x, delta_y = wandering.walk()
+        actual_location = self.wandering_location[wandering]
+        new_location = actual_location.move(delta_x, delta_y)
+
+        self.wandering_location[wandering] = new_location
+
+    def get_location(self, wandering):
+            return self.wandering_location[wandering]
