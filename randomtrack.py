e58bdb6bb7fd6bdbf6a2325fd123a53e172d3eeb
hspatino <hspatino2@misena.edu.co>
2022-02-22 15:12:23 -0500

    Creamos la función simular caminata y la función gráficar en el enter point

--- randomtrack.py
+++ randomtrack.py
@@ -1,4 +1,5 @@
 import imp
+from turtle import title
 from wandering import CommonWandering
 from track import Track
 from location import Location
@@ -12,3 +13,18 @@ def walking(location, wandering, steps):
         location.move_wandering(wandering)
 
     return beggining.distance(location.get_location(wandering))
+
+def simulate_walk(steps, number_attemps, wandering_type):
+    wandering = wandering_type(name='Alirio')
+    origin = Location(0, 0)
+    distance = []
+
+    for _ in range(number_attemps):
+        track = Track()
+        track.add_wandering(wandering, origin)
+        simulation_walk = walking(track, wandering, steps)
+        distance.append(round(simulation_walk, 1))
+
+def graph(x, y):
+    graphics = figure(title='Camino del Errante', x_axis_label='Pasos', y_axis_label='Distancia')
+    graphics.line(x, y, legend='Distancia')
