ad346b07235c9dfd1cd0535a4246dde72d49dc8b
hspatino <hspatino2@misena.edu.co>
2022-03-15 15:02:11 -0500

    Realizamos cambios en el trayecto del errante

--- wandering.py
+++ wandering.py
@@ -1,9 +1,18 @@
 import random
+from re import X
 
 class Wandering:
 
-    def __init__(self, name):
+    def __init__(self, name, x=0, y=0):
         self.name = name
+        self.x= x
+        self.y= y
+
+    def position(self):
+        return (self.x, self.y)
+
+    def distance_origin(self):
+        return (self.x**2, self.y**2)**0.5
 
 class CommonWandering(Wandering):
 
@@ -11,4 +20,9 @@ class CommonWandering(Wandering):
         super().__init__(name)
 
     def walk(self):
-        return random.choice([(0, 2), (0,-2), (2, 0), (-2, 0)])
+        dx, dy = random.choice([(-6, 2), (5,-2), (2, 3), (1, 7)])
+        self.x += dx
+        self.y += dy
+        return [dx, dy]
+
+        
